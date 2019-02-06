574c035fd50902e4195d437b16c5e9cdbd001777
Kian Davoudi-Rad <kian@Kians-MacBook-Pro.local>
2019-02-05 20:43:17 -0800

    adding clearn project

---
+++ Chapter2/LinkedList/Q2.1/C/main.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+struct  Node {
+struct Node *next;
+int data;
+} Node;
+
+int main(){
+
+struct Node *head = (struct Node*) malloc(sizeof(struct Node));
+
+    
+return 0;
+    
+}
